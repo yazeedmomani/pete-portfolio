66f7aa224253f9a3808a9aaca0a86a53331ac75e
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2022-12-01 10:49:40 +0300

    continue working on form

--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -3,7 +3,7 @@ import styles from "./ContactMeSection.module.css";
 const LandingSection = (props) => {
   return (
     <div className={styles.section}>
-        <h1 className={styles.header}>Contact Me</h1>
+      <h1 className={styles.header}>Contact Me</h1>
       <form className={styles.form}>
         <label
           htmlFor="name"
@@ -15,6 +15,35 @@ const LandingSection = (props) => {
           id="name"
           className={styles.input}
         />
+        <label
+          htmlFor="email"
+          className={styles.label}>
+          Email
+        </label>
+        <input
+          type="text"
+          id="email"
+          className={styles.input}
+        />
+        <label
+          htmlFor="type"
+          className={styles.label}>
+          Name
+        </label>
+        <input
+          type="text"
+          id="type"
+          className={styles.input}
+        />
+        <label
+          htmlFor="message"
+          className={styles.label}>
+          Message
+        </label>
+        <textarea
+          name="message"
+          id="message"
+          className={`${styles.input} ${styles.textarea}`}></textarea>
       </form>
     </div>
   );
