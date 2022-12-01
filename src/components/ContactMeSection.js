35646ef3764b02ad2c9b8794d6c5e602afdabe57
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2022-12-01 10:43:45 +0300

    add form styles

--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -1,7 +1,23 @@
-import styles from './ContactMeSection.module.css'
+import styles from "./ContactMeSection.module.css";
 
 const LandingSection = (props) => {
-  return <div className={styles.section}></div>
+  return (
+    <div className={styles.section}>
+        <h1 className={styles.header}>Contact Me</h1>
+      <form className={styles.form}>
+        <label
+          htmlFor="name"
+          className={styles.label}>
+          Name
+        </label>
+        <input
+          type="text"
+          id="name"
+          className={styles.input}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default LandingSection;
